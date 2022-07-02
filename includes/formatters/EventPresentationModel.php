5bb3a2466d134269062812fd1f07d037fa87352c
Reedy <reedy@wikimedia.org>
2022-07-02 15:35:22 +0000

    Add return type to jsonSerialize()
    Bug: T311919
    Change-Id: Ie16d4e175250b5239e33026046adfb4ab9097bd1
    (cherry picked from commit b86fb539fa2973c4e32f073f6a666ebd0db22c56)

--- includes/formatters/EventPresentationModel.php
+++ includes/formatters/EventPresentationModel.php
@@ -532,8 +532,7 @@ abstract class EchoEventPresentationModel implements JsonSerializable {
 	 * @return array
 	 * @throws TimestampException
 	 */
-	#[ReturnTypeWillChange]
-	public function jsonSerialize() {
+	public function jsonSerialize(): array {
 		$body = $this->getBodyMessage();
 
 		return [
